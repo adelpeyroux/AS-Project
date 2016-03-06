41c14e604ea2b0e9de5eee32b8e291d057d0d58e
Rack07 <rack.kendorim@gmail.com>
2016-03-06 18:27:03 +0100

    MAJ de l'analyse lexicale Deux etat initial et text literal. EN LITTERAL on a séparé les mots des autres points mais ce n'est pas obligatoire en réalité prendre tout le bloc entre guillemet serait suffisant. (les guillemets echappé En INITIAL On récupère les débuts de balises les arguments et différents signes servant a structurer le fichier on en profite pour virer les espaces inutiles et les sauts de lignes abusifs

--- header/tree.h
+++ header/tree.h
@@ -7,23 +7,23 @@
 
 struct s_tree;
 struct s_attributes;
-enum type {t_tree, t_word};        //typage des nœuds: permet de savoir si un nœud construit
-                               //un arbre ou s'il s'agit simplement de texte
+enum type {t_tree, t_word};		//typage des nœuds: permet de savoir si un nœud construit
+								//un arbre ou s'il s'agit simplement de texte
 							   
 struct s_attributes{
-    char * key;               //nom de l'attribut
-    char * value;             //valeur de l'attribut
-    struct s_attributes * next; //attribut suivant
+    char * key;					//nom de l'attribut
+    char * value;				//valeur de l'attribut
+    struct s_attributes * next;	//attribut suivant
 };
 
 struct s_tree {
-    char * label;              //étiquette du nœud
-    bool nullary;              //nœud vide, par exemple <br/>
-    bool space;                //nœud suivi d'un espace
-    enum type tp;              //type du nœud. nullary doit être tr	ue s tp vaut word
-    struct s_attributes * attr;  //attributs du nœud
-    struct s_tree * daughters;   //fils gauche, qui doit être NULL si nullary est true
-    struct s_tree * right;       //frère droit
+    char * label;				//étiquette du nœud
+    bool nullary;				//nœud vide, par exemple <br/>
+    bool space;					//nœud suivi d'un espace
+    enum type tp;				//type du nœud. nullary doit être tr	ue s tp vaut word
+    struct s_attributes * attr;	//attributs du nœud
+    struct s_tree * daughters;	//fils gauche, qui doit être NULL si nullary est true
+    struct s_tree * right;		//frère droit
 };
 
 typedef struct s_tree * tree;
